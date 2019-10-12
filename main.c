ac2f416f5e3186941446caea6263eeb00eceaf45
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2019-10-13 02:08:05 +0700

    added epoll creation and destruction

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+//BEGIN EPOLL
+#include<sys/epoll.h>
+//END EPOLL
 //BEGIN OPEN LISTENING SOCKET
 #include <sys/socket.h>
 #include <unistd.h>
@@ -24,21 +27,25 @@ int main(int argc, char**argv)
     perror("Error on accepting connection");
     return -1;
   }
+
+  int const epollFd = epoll_create(1);
+  if (epollFd < 0) {
+    perror("Error creating epoll object");
+    return -1;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
   ssize_t const len = recv(con, buffer, 100, 0);
-  if(con < 0){
+  if(len < 0){
     perror("Error on Reading message");
     return -1;
   }
   buffer[len] = '\0';
   printf("Message is\n%s\n", buffer);
-  ssize_t const sended = send(con, buffer, (size_t)len, 0);
-  if(sended < 0){
-    perror("Error on send echo");
-  }
-  if(sended != len) {
-    fprintf(stderr, "incorrect sended data. expected %zd, sended %zd\n", len, sended);
-  }
   close(con);
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
   //OPENING THE OTHER END
   int const target = send_socket_gen("localhost", "8000");
   if(target < 0){
@@ -53,6 +60,8 @@ int main(int argc, char**argv)
   if(sended_target != len) {
     fprintf(stderr, "incorrect sended data. expected %zd, sended %zd\n", len, sended_target);
   }
+
+  close(epollFd);
   close(target);
   return 0;
 }
